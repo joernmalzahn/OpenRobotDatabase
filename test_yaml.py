6df0168d184624a27c74583aaa6df070af7e772c
Jörn Malzahn <10830038+joernmalzahn@users.noreply.github.com>
2020-02-20 14:27:31 +0100

    Added vendor schema. Schema is checked by test_yaml.py and exits without errors.

--- test_yaml.py
+++ test_yaml.py
@@ -1,22 +1,37 @@
 import yaml
 import os
+import Rx
 
 cobotPath = 'cobots' + os.sep
 allCobotYamls = os.listdir(cobotPath)
 print(allCobotYamls)
 
+rx = Rx.Factory({ "register_core_types": True })
+
+schemaPath = cobotPath + "cobot_schema.yaml"
+schema = yaml.load(open(schemaPath), Loader=yaml.FullLoader)
+validator = rx.make_schema(schema)
+
 for file in allCobotYamls:
-    with open(cobotPath + file) as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        print(data)
-
-
-print(data['mechanical properties']['mounting'])
-print(data['installation time'])
+    if file != "cobot_schema.yaml" and file != "cobot_template.yaml":
+        with open(cobotPath + file) as f:
+            print(file)
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            validator.validate(data)
+            print(data)
 
 vendorPath = 'vendors' + os.sep
 allVendorYamls = os.listdir(vendorPath)
+
+schemaPath = vendorPath + "vendor_schema.yaml"
+schema = yaml.load(open(schemaPath), Loader=yaml.FullLoader)
+validator = rx.make_schema(schema)
+
 for file in allVendorYamls:
-    with open(vendorPath + file) as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        print(data)
+    if file != "vendor_schema.yaml" and file != "vendor_template.yaml":
+        with open(vendorPath + file) as f:
+            print(file)
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            validator.validate(data)
+            print(data)
+
